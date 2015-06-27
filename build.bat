af757a5a836c247c11283aded77ee408d1844402
justcoding121 <jehonathant@gmail.com>
2015-06-27 19:56:56 -0400

    Fix build script

--- build.bat
+++ build.bat
@@ -14,10 +14,8 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
  
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Titanium.Web.Proxy /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Titanium.Web.Proxy.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
 mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net40
  
 %nuget% pack "Titanium.Web.Proxy.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
