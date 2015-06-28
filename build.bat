e0c4b3a33742f8800be26ab15ecfdaac147ffb83
justcoding121 <jehonathant@gmail.com>
2015-06-27 20:15:03 -0400

    auto nuget restore

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ if not "%PackageVersion%" == "" (
  
 set nuget=
 if "%nuget%" == "" (
-	set nuget=nuget
+	set nuget=.nuget\nuget.exe
 )
  
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Titanium.Web.Proxy.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
