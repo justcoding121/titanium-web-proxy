033dff0537f477931e10cc29b3f002f98a4e0743
titanium007 <jehonathant@gmail.com>
2015-07-08 15:34:22 -0400

    Replace fiddler root with another test root; Fix nuspec

--- build.bat
+++ build.bat
@@ -17,5 +17,7 @@ if "%nuget%" == "" (
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Titanium.Web.Proxy.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
 mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
  
 %nuget% pack "Titanium.Web.Proxy.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
