23f1d1816e7ca08ec97da516e973891c02fede4a
justcoding121 <jehonathant@gmail.com>
2015-09-04 21:37:21 -0400

    Fix request body modification

---
+++ Titanium.Web.Proxy/Exceptions/BodyNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Titanium.Web.Proxy.Exceptions
+{
+    public class BodyNotFoundException : Exception
+    {
+        public BodyNotFoundException(string message)
+            :base(message)
+        {
+
+        }
+     
+    }
+}
