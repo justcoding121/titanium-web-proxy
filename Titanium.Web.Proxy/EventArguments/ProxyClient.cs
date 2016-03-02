dee6d59f127ac19c17cb649cc28941c504c33697
titanium007 <jehonathant@gmail.com>
2016-03-02 17:31:35 -0500

    add comments

---
+++ Titanium.Web.Proxy/EventArguments/ProxyClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using Titanium.Web.Proxy.Helpers;
+
+namespace Titanium.Web.Proxy.EventArguments
+{
+    /// <summary>
+    /// This class wraps Tcp connection to Server
+    /// </summary>
+    public class ProxyClient
+    {
+        /// <summary>
+        /// TcpClient used to communicate with server
+        /// </summary>
+        internal TcpClient TcpClient { get; set; }
+
+        /// <summary>
+        /// holds the stream to server
+        /// </summary>
+        internal Stream ClientStream { get; set; }
+
+        /// <summary>
+        /// Used to read line by line from server
+        /// </summary>
+        internal CustomBinaryReader ClientStreamReader { get; set; }
+
+        /// <summary>
+        /// used to write line by line to server
+        /// </summary>
+        internal StreamWriter ClientStreamWriter { get; set; }
+
+    }
+}
--- Titanium.Web.Proxy/Exceptions/BodyNotFoundException.cs
+++ Titanium.Web.Proxy/Exceptions/BodyNotFoundException.cs
@@ -2,6 +2,9 @@
 
 namespace Titanium.Web.Proxy.Exceptions
 {
+    /// <summary>
+    /// An expception thrown when body is unexpectedly empty
+    /// </summary>
     public class BodyNotFoundException : Exception
     {
         public BodyNotFoundException(string message)
