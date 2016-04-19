0c997fd76cec8194f37d5378b8893747e915c9f4
justcoding121 <jehonathant@gmail.com>
2016-04-19 08:26:15 +0530

    Add Expect100Continue disable option
    For servers/clients having buggy implementation

--- Titanium.Web.Proxy/RequestHandler.cs
+++ Titanium.Web.Proxy/RequestHandler.cs
@@ -266,6 +266,21 @@ namespace Titanium.Web.Proxy
                     args.ProxySession.SetConnection(connection);
                     args.ProxySession.SendRequest();
 
+                    if(Enable100ContinueBehaviour)
+                    if (args.ProxySession.Request.Is100Continue)
+                    {
+                        WriteResponseStatus(args.ProxySession.Response.HttpVersion, "100",
+                                "Continue", args.Client.ClientStreamWriter);
+                        args.Client.ClientStreamWriter.WriteLine();
+                    }
+                    else if (args.ProxySession.Request.ExpectationFailed)
+                    {
+                        WriteResponseStatus(args.ProxySession.Response.HttpVersion, "417",
+                                "Expectation Failed", args.Client.ClientStreamWriter);
+                        args.Client.ClientStreamWriter.WriteLine();
+                    }
+
+
                     //If request was modified by user
                     if (args.ProxySession.Request.RequestBodyRead)
                     {
@@ -275,14 +290,20 @@ namespace Titanium.Web.Proxy
                     }
                     else
                     {
-                        //If its a post/put request, then read the client html body and send it to server
-                        if (httpMethod.ToUpper() == "POST" || httpMethod.ToUpper() == "PUT")
+                        if (!args.ProxySession.Request.ExpectationFailed)
                         {
-                            SendClientRequestBody(args);
+                            //If its a post/put request, then read the client html body and send it to server
+                            if (httpMethod.ToUpper() == "POST" || httpMethod.ToUpper() == "PUT")
+                            {
+                                SendClientRequestBody(args);
+                            }
                         }
                     }
 
-                    HandleHttpSessionResponse(args);
+                    if (!args.ProxySession.Request.ExpectationFailed)
+                    {
+                        HandleHttpSessionResponse(args);
+                    }
 
                     //if connection is closing exit
                     if (args.ProxySession.Response.ResponseKeepAlive == false)
--- Titanium.Web.Proxy/Responses/OkResponse.cs
+++ Titanium.Web.Proxy/Responses/OkResponse.cs
@@ -10,12 +10,6 @@ namespace Titanium.Web.Proxy.Responses
         {
             ResponseStatusCode = "200";
             ResponseStatusDescription = "Ok";
-
-            ResponseHeaders.Add(new HttpHeader("Timestamp", DateTime.Now.ToString()));
-            ResponseHeaders.Add(new HttpHeader("content-length", DateTime.Now.ToString()));
-            ResponseHeaders.Add(new HttpHeader("Cache-Control", "no-cache, no-store, must-revalidate"));
-            ResponseHeaders.Add(new HttpHeader("Pragma", "no-cache"));
-            ResponseHeaders.Add(new HttpHeader("Expires", "0"));
         }
     }
 }
