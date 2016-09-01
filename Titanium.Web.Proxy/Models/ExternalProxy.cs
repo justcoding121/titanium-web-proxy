95fe442892c25e9e17e6cb47514a7dae90f1cf56
ilushka85 <ibeyrak@channeliq.com>
2016-09-01 09:25:10 -0500

    Create proper support for Upstream proxy supporting username, password, and ssl fixes

--- Titanium.Web.Proxy/Models/ExternalProxy.cs
+++ Titanium.Web.Proxy/Models/ExternalProxy.cs
@@ -5,6 +5,8 @@
     /// </summary>
     public class ExternalProxy
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string HostName { get; set; }
         public int Port { get; set; }
     }
--- Titanium.Web.Proxy/Network/TcpConnectionFactory.cs
+++ Titanium.Web.Proxy/Network/TcpConnectionFactory.cs
@@ -7,6 +7,7 @@ using System.Net.Security;
 using Titanium.Web.Proxy.Helpers;
 using Titanium.Web.Proxy.Models;
 using System.Security.Authentication;
+using System.Linq;
 
 namespace Titanium.Web.Proxy.Network
 {
@@ -34,7 +35,7 @@ namespace Titanium.Web.Proxy.Network
         /// <param name="clientStream"></param>
         /// <returns></returns>
         internal async Task<TcpConnection> CreateClient(int bufferSize, int connectionTimeOutSeconds,
-            string remoteHostName, int remotePort, Version httpVersion, 
+            string remoteHostName, int remotePort, Version httpVersion,
             bool isHttps, SslProtocols supportedSslProtocols,
             RemoteCertificateValidationCallback remoteCertificateValidationCallback, LocalCertificateSelectionCallback localCertificateSelectionCallback,
             ExternalProxy externalHttpProxy, ExternalProxy externalHttpsProxy,
@@ -55,9 +56,15 @@ namespace Titanium.Web.Proxy.Network
 
                     using (var writer = new StreamWriter(stream, Encoding.ASCII, bufferSize, true))
                     {
-                        await writer.WriteLineAsync(string.Format("CONNECT {0}:{1} {2}", remoteHostName, remotePort, httpVersion));
+                        await writer.WriteLineAsync(string.Format("CONNECT {0}:{1} HTTP/{2}", remoteHostName, remotePort,httpVersion));
                         await writer.WriteLineAsync(string.Format("Host: {0}:{1}", remoteHostName, remotePort));
                         await writer.WriteLineAsync("Connection: Keep-Alive");
+
+                        if (externalHttpsProxy.UserName != null && externalHttpsProxy.UserName != "" && externalHttpsProxy.Password != null)
+                        {
+                            await writer.WriteLineAsync("Proxy-Connection: keep-alive");
+                            await writer.WriteLineAsync("Proxy-Authorization" + ": Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(externalHttpsProxy.UserName + ":" + externalHttpsProxy.Password)));
+                        }
                         await writer.WriteLineAsync();
                         await writer.FlushAsync();
                         writer.Close();
@@ -67,7 +74,8 @@ namespace Titanium.Web.Proxy.Network
                     {
                         var result = await reader.ReadLineAsync();
 
-                        if (!result.ToLower().Contains("200 connection established"))
+
+                        if (!new string[] { "200 OK", "connection established" }.Any(s=> result.ToLower().Contains(s.ToLower())))
                         {
                             throw new Exception("Upstream proxy failed to create a secure tunnel");
                         }
@@ -119,15 +127,18 @@ namespace Titanium.Web.Proxy.Network
 
             stream.ReadTimeout = connectionTimeOutSeconds * 1000;
             stream.WriteTimeout = connectionTimeOutSeconds * 1000;
-           
+
             return new TcpConnection()
             {
+                UpStreamHttpProxy = externalHttpProxy,
+                UpStreamHttpsProxy = externalHttpsProxy,
                 HostName = remoteHostName,
                 port = remotePort,
                 IsHttps = isHttps,
                 TcpClient = client,
                 StreamReader = new CustomBinaryReader(stream),
-                Stream = stream
+                Stream = stream,
+                Version = httpVersion
             };
         }
     }
