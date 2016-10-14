a44450a6401cfd798e285b32b2ec9578fb61a692
Hakan Arıcı <hakan@netsparker.com>
2016-10-14 16:47:25 +0300

    UseDefaultCredentials property is added to ExternalProxy.

--- Titanium.Web.Proxy/Models/ExternalProxy.cs
+++ Titanium.Web.Proxy/Models/ExternalProxy.cs
@@ -1,12 +1,47 @@
-﻿namespace Titanium.Web.Proxy.Models
+﻿using System;
+using System.Net;
+
+namespace Titanium.Web.Proxy.Models
 {
     /// <summary>
     /// An upstream proxy this proxy uses if any
     /// </summary>
     public class ExternalProxy
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        private static readonly Lazy<NetworkCredential> DefaultCredentials = new Lazy<NetworkCredential>(() => CredentialCache.DefaultNetworkCredentials);
+
+        private string userName;
+        private string password;
+        
+        public bool UseDefaultCredentials { get; set; }
+
+        public string UserName {
+            get { return UseDefaultCredentials ? DefaultCredentials.Value.UserName : userName; }
+            set
+            {
+                userName = value;
+
+                if (DefaultCredentials.Value.UserName != userName)
+                {
+                    UseDefaultCredentials = false;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return UseDefaultCredentials ? DefaultCredentials.Value.Password : password; }
+            set
+            {
+                password = value;
+
+                if (DefaultCredentials.Value.Password != password)
+                {
+                    UseDefaultCredentials = false;
+                }
+            }
+        }
+
         public string HostName { get; set; }
         public int Port { get; set; }
     }
