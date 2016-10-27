9a93ca0ddfa9ce0e1a67d083ddcfc54084b8c70a
Nordin Rahman <nordin.rahman@gmail.com>
2016-10-27 10:52:55 +0800

    Rename ProxyHttpException.SessionInfo to SessionEventArgs

--- Titanium.Web.Proxy/Exceptions/ProxyException.cs
+++ Titanium.Web.Proxy/Exceptions/ProxyException.cs
@@ -7,6 +7,11 @@ namespace Titanium.Web.Proxy.Exceptions
     /// </summary>
     public abstract class ProxyException : Exception
     {
+        /// <summary>
+        /// Instantiate this exception - must be invoked by derived classes' constructors
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         protected ProxyException(string message, Exception innerException) : base(message, innerException)
         {
         }
