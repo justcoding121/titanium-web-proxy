90c935ea6116e4a4b5d5867548ec401a1fbefbb8
Honfika <honfika@gmail.com>
2017-05-16 00:13:03 +0200

    - Create less TcpRow objects - Use static Http Verion objects - Common HTTP header parser (request, response) - Code duplication removed from ProxyAuthorizationHandler - Fix? for issue #227

--- Titanium.Web.Proxy/Extensions/TcpExtensions.cs
+++ Titanium.Web.Proxy/Extensions/TcpExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Sockets;
+using Titanium.Web.Proxy.Helpers;
 
 namespace Titanium.Web.Proxy.Extensions
 {
@@ -31,6 +32,26 @@ namespace Titanium.Web.Proxy.Extensions
             {
                 client.Blocking = blockingState;
             }
+        }
+
+        /// <summary>
+        /// Gets the local port from a native TCP row object.
+        /// </summary>
+        /// <param name="tcpRow">The TCP row.</param>
+        /// <returns>The local port</returns>
+        internal static int GetLocalPort(this NativeMethods.TcpRow tcpRow)
+        {
+            return (tcpRow.localPort1 << 8) + tcpRow.localPort2 + (tcpRow.localPort3 << 24) + (tcpRow.localPort4 << 16);
+        }
+
+        /// <summary>
+        /// Gets the remote port from a native TCP row object.
+        /// </summary>
+        /// <param name="tcpRow">The TCP row.</param>
+        /// <returns>The remote port</returns>
+        internal static int GetRemotePort(this NativeMethods.TcpRow tcpRow)
+        {
+            return (tcpRow.remotePort1 << 8) + tcpRow.remotePort2 + (tcpRow.remotePort3 << 24) + (tcpRow.remotePort4 << 16);
         }
     }
 }
