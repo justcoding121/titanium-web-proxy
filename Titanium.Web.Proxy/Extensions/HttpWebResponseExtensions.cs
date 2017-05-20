f0232896ec80ff60369cd0c75f6a7d42d4f63f1c
Honfika <honfika@gmail.com>
2017-05-20 22:06:54 +0200

    Content type fixed in request header, too (moved to a common place)

--- Titanium.Web.Proxy/Extensions/HttpWebResponseExtensions.cs
+++ Titanium.Web.Proxy/Extensions/HttpWebResponseExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text;
+using Titanium.Web.Proxy.Helpers;
 using Titanium.Web.Proxy.Http;
-using Titanium.Web.Proxy.Shared;
 
 namespace Titanium.Web.Proxy.Extensions
 {
@@ -14,45 +14,7 @@ namespace Titanium.Web.Proxy.Extensions
         /// <returns></returns>
         internal static Encoding GetResponseCharacterEncoding(this Response response)
         {
-            try
-            {
-                //return default if not specified
-                if (response.ContentType == null)
-                {
-                    return Encoding.GetEncoding("ISO-8859-1");
-                }
-
-                //extract the encoding by finding the charset
-                var contentTypes = response.ContentType.Split(ProxyConstants.SemiColonSplit);
-                foreach (var contentType in contentTypes)
-                {
-                    var encodingSplit = contentType.Split(ProxyConstants.EqualSplit, 2);
-                    if (encodingSplit.Length == 2 && encodingSplit[0].Trim().Equals("charset", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        string value = encodingSplit[1];
-                        if (value.Equals("x-user-defined", StringComparison.OrdinalIgnoreCase))
-                        {
-                            //todo: what is this?
-                            continue;
-                        }
-
-                        if (value.Length > 2 && value[0] == '"' && value[value.Length - 1] == '"')
-                        {
-                            value = value.Substring(1, value.Length - 2);
-                        }
-
-                        return Encoding.GetEncoding(value);
-                    }
-                }
-            }
-            catch
-            {
-                //parsing errors
-                // ignored
-            }
-
-            //return default if not specified
-            return Encoding.GetEncoding("ISO-8859-1");
+            return HeaderHelper.GetEncodingFromContentType(response.ContentType);
         }
     }
 }
