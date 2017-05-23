a81dada049a5925c9d7f4fa4bd73d9fd15b373fe
PDR\jehonathan.thomas <jehonathan.thomas@connectiverx.com>
2017-05-23 13:29:08 -0400

    cleanup

--- Titanium.Web.Proxy/Helpers/RunTime.cs
+++ Titanium.Web.Proxy/Helpers/RunTime.cs
@@ -7,13 +7,15 @@ namespace Titanium.Web.Proxy.Helpers
     /// </summary>
     internal class RunTime
     {
+        private static Lazy<bool> isMonoRuntime 
+            = new Lazy<bool>(()=> Type.GetType("Mono.Runtime") != null);
         /// <summary>
         /// Checks if current run time is Mono
         /// </summary>
         /// <returns></returns>
         internal static bool IsRunningOnMono()
         {
-            return Type.GetType("Mono.Runtime") != null;
+            return isMonoRuntime.Value;
         }
     }
 }
