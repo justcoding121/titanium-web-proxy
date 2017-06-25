b553c6ac5dc3cee57af4fdca1fe90106d2ea4c95
Honfika <honfika@gmail.com>
2017-06-25 10:18:57 +0200

    Websocket fix in GUI demo + handle user exceptions in TWP

--- Titanium.Web.Proxy/Extensions/FuncExtensions.cs
+++ Titanium.Web.Proxy/Extensions/FuncExtensions.cs
@@ -18,17 +18,30 @@ namespace Titanium.Web.Proxy.Extensions
             Task.WhenAll(handlerTasks).Wait();
         }
 
-        public static async Task InvokeParallelAsync<T>(this Func<object, T, Task> callback, object sender, T args)
+        public static async Task InvokeParallelAsync<T>(this Func<object, T, Task> callback, object sender, T args, Action<Exception> exceptionFunc)
         {
             var invocationList = callback.GetInvocationList();
             var handlerTasks = new Task[invocationList.Length];
 
             for (int i = 0; i < invocationList.Length; i++)
             {
-                handlerTasks[i] = ((Func<object, T, Task>)invocationList[i])(sender, args);
+                handlerTasks[i] = InvokeAsync((Func<object, T, Task>)invocationList[i], sender, args, exceptionFunc);
             }
 
             await Task.WhenAll(handlerTasks);
+        }
+
+        private static async Task InvokeAsync<T>(Func<object, T, Task> callback, object sender, T args, Action<Exception> exceptionFunc)
+        {
+            try
+            {
+                await callback(sender, args);
+            }
+            catch (Exception ex)
+            {
+                var ex2 = new Exception("Exception thrown in user event", ex);
+                exceptionFunc(ex2);
+            }
         }
     }
 }
