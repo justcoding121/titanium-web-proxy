5a99d3e7cbbaeabcaaa30eca56bc1781239dd161
Honfika <honfika@gmail.com>
2017-06-29 18:13:17 +0200

    Parse server hello + minor impovements

--- Titanium.Web.Proxy/Ssl/SslTools.cs
+++ Titanium.Web.Proxy/Ssl/SslTools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Titanium.Web.Proxy.Helpers;
 
@@ -108,26 +109,7 @@ namespace Titanium.Web.Proxy.Ssl
 
                 byte[] compressionData = peekStream.ReadBytes(length);
 
-                List<SslExtension> extensions = null;
-                if (majorVersion > 3 || majorVersion == 3 && minorVersion >= 1)
-                {
-                    if (await peekStream.EnsureBufferLength(2))
-                    {
-                        length = peekStream.ReadInt16();
-
-                        if (await peekStream.EnsureBufferLength(length))
-                        {
-                            extensions = new List<SslExtension>();
-                            while (peekStream.Available > 3)
-                            {
-                                int id = peekStream.ReadInt16();
-                                length = peekStream.ReadInt16();
-                                byte[] data = peekStream.ReadBytes(length);
-                                extensions.Add(SslExtensions.GetExtension(id, data));
-                            }
-                        }
-                    }
-                }
+                var extensions = await ReadExtensions(majorVersion, minorVersion, peekStream);
 
                 var clientHelloInfo = new ClientHelloInfo
                 {
@@ -146,6 +128,33 @@ namespace Titanium.Web.Proxy.Ssl
             return null;
         }
 
+        private static async Task<List<SslExtension>> ReadExtensions(int majorVersion, int minorVersion, CustomBufferedPeekStream peekStream)
+        {
+            List<SslExtension> extensions = null;
+            if (majorVersion > 3 || majorVersion == 3 && minorVersion >= 1)
+            {
+                if (await peekStream.EnsureBufferLength(2))
+                {
+                    int extensionsLength = peekStream.ReadInt16();
+
+                    if (await peekStream.EnsureBufferLength(extensionsLength))
+                    {
+                        extensions = new List<SslExtension>();
+                        while (extensionsLength > 3)
+                        {
+                            int id = peekStream.ReadInt16();
+                            int length = peekStream.ReadInt16();
+                            byte[] data = peekStream.ReadBytes(length);
+                            extensions.Add(SslExtensions.GetExtension(id, data));
+                            extensionsLength -= 4 + length;
+                        }
+                    }
+                }
+            }
+
+            return extensions;
+        }
+
         public static async Task<bool> IsServerHello(CustomBufferedStream serverStream)
         {
             var serverHello = await GetServerHelloInfo(serverStream);
@@ -154,8 +163,6 @@ namespace Titanium.Web.Proxy.Ssl
 
         public static async Task<ServerHelloInfo> GetServerHelloInfo(CustomBufferedStream serverStream)
         {
-            //todo: THIS IS NOT READY YET
-
             //detects the HTTPS ClientHello message as it is described in the following url:
             //https://stackoverflow.com/questions/3897883/how-to-detect-an-incoming-ssl-https-handshake-ssl-wire-format
 
@@ -200,9 +207,13 @@ namespace Titanium.Web.Proxy.Ssl
 
                 int length = peekStream.ReadInt16();
 
-                if (peekStream.ReadByte() != 0x01)
+                var ps0 = new CustomBufferedPeekStream(serverStream, 5);
+                var data0 = ps0.ReadBytes(length);
+                var str = string.Join(" ", data0.Select(x=>x.ToString("X2")));
+
+                if (peekStream.ReadByte() != 0x02)
                 {
-                    // should be ClientHello
+                    // should be ServerHello
                     return null;
                 }
 
@@ -214,63 +225,18 @@ namespace Titanium.Web.Proxy.Ssl
                 byte[] random = peekStream.ReadBytes(32);
                 length = peekStream.ReadByte();
 
-                // sessionid + 2 ciphersData length
-                if (!await peekStream.EnsureBufferLength(length + 2))
+                // sessionid + cipherSuite + compressionMethod
+                if (!await peekStream.EnsureBufferLength(length + 2 + 1))
                 {
                     return null;
                 }
 
                 byte[] sessionId = peekStream.ReadBytes(length);
 
-                length = peekStream.ReadInt16();
+                int cipherSuite = peekStream.ReadInt16();
+                byte compressionMethod = peekStream.ReadByte();
 
-                // ciphersData + compressionData length
-                if (!await peekStream.EnsureBufferLength(length + 1))
-                {
-                    return null;
-                }
-
-                byte[] ciphersData = peekStream.ReadBytes(length);
-                int[] ciphers = new int[ciphersData.Length / 2];
-                for (int i = 0; i < ciphers.Length; i++)
-                {
-                    ciphers[i] = (ciphersData[2 * i] << 8) + ciphersData[2 * i + 1];
-                }
-
-                length = peekStream.ReadByte();
-                if (length < 1)
-                {
-                    return null;
-                }
-
-                // compressionData
-                if (!await peekStream.EnsureBufferLength(length))
-                {
-                    return null;
-                }
-
-                byte[] compressionData = peekStream.ReadBytes(length);
-
-                List<SslExtension> extensions = null;
-                if (majorVersion > 3 || majorVersion == 3 && minorVersion >= 1)
-                {
-                    if (await peekStream.EnsureBufferLength(2))
-                    {
-                        length = peekStream.ReadInt16();
-
-                        if (await peekStream.EnsureBufferLength(length))
-                        {
-                            extensions = new List<SslExtension>();
-                            while (peekStream.Available > 3)
-                            {
-                                int id = peekStream.ReadInt16();
-                                length = peekStream.ReadInt16();
-                                byte[] data = peekStream.ReadBytes(length);
-                                extensions.Add(SslExtensions.GetExtension(id, data));
-                            }
-                        }
-                    }
-                }
+                var extensions = await ReadExtensions(majorVersion, minorVersion, peekStream);
 
                 var serverHelloInfo = new ServerHelloInfo
                 {
@@ -278,8 +244,8 @@ namespace Titanium.Web.Proxy.Ssl
                     MinorVersion = minorVersion,
                     Random = random,
                     SessionId = sessionId,
-                    Ciphers = ciphers,
-                    CompressionData = compressionData,
+                    CipherSuite = cipherSuite,
+                    CompressionMethod = compressionMethod,
                     Extensions = extensions,
                 };
 
