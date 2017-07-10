fa24180be79f86782182c0e71d9236bf6620b82a
justcoding121 <jehonathant@gmail.com>
2017-07-10 19:21:18 -0400

    experimental build failure fix

--- .build/default.ps1
+++ .build/default.ps1
@@ -31,16 +31,17 @@ $NuGet = Join-Path $SolutionRoot ".nuget\nuget.exe"
 
 $MSBuild14 = "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\msbuild.exe"
 
-$MSBuild = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
-$MSBuild -replace ' ', '` '
-
+$MSBuild  = & $Here\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+if ($MSBuild) {
+  $MSBuild  = join-path $MSBuild 'MSBuild\15.0\Bin\MSBuild.exe'
+}
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
 Task default -depends Clean, Build, Package
 
 Task Build {
-	exec { . $MSBuild14 $SolutionFile14 /t:Build /v:normal /p:Configuration=$Configuration  }
+	
      & $MSBuild $SolutionFile /t:Build /v:normal /p:Configuration=$Configuration  
 }
 
@@ -50,14 +51,14 @@ Task Package -depends Build {
 
 Task Clean -depends Install-BuildTools {
 	Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { Remove-Item $_.fullname -Force -Recurse }
-    exec { . $MSBuild14 $SolutionFile14 /t:Clean /v:quiet }
+    
 	 & $MSBuild $SolutionFile /t:Clean /v:quiet 
 
 }
 
 
 Task Install-MSBuild {
-    if(!(Test-Path $MSBuild14)) 
+    if(!(Test-Path $MSBuild14) -Or !(Test-Path $MSBuild)) 
 	{ 
 		cinst microsoft-build-tools -y
 	}
