4507f1b4d2db07dfd260c734b9f1f96d0f83e21f
Honfika <honfika@gmail.com>
2018-01-02 17:35:46 +0100

    Exclude (for example) Google cloud messaging streams. It is a https tunnel, but the content is not http

---
+++ Titanium.Web.Proxy/Network/DebugCustomBufferedStream.cs
@@ -0,0 +1,49 @@
+﻿#if DEBUG
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using StreamExtended.Network;
+
+namespace Titanium.Web.Proxy.Network
+{
+    class DebugCustomBufferedStream : CustomBufferedStream
+    {
+        private const string basePath = @"c:\\11\\";
+
+        private static int counter;
+
+        public int Counter { get; }
+
+        private FileStream fileStreamSent;
+
+        private FileStream fileStreamReceived;
+
+        public DebugCustomBufferedStream(Stream baseStream, int bufferSize) : base(baseStream, bufferSize)
+        {
+            Counter = Interlocked.Increment(ref counter);
+            fileStreamSent = new FileStream(Path.Combine(basePath, $"{Counter}_sent.dat"), FileMode.Create);
+            fileStreamReceived = new FileStream(Path.Combine(basePath, $"{Counter}_received.dat"), FileMode.Create);
+        }
+
+        protected override void OnDataSent(byte[] buffer, int offset, int count)
+        {
+            fileStreamSent.Write(buffer, offset, count);
+        }
+
+        protected override void OnDataReceived(byte[] buffer, int offset, int count)
+        {
+            fileStreamReceived.Write(buffer, offset, count);
+        }
+
+        public void Flush()
+        {
+            fileStreamSent.Flush(true);
+            fileStreamReceived.Flush(true);
+        }
+    }
+}
+#endif
