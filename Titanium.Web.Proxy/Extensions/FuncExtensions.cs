41fb34f3f3e4d1a9a22aff384d8dc53916b7f7d4
justcoding121 <jehonathant@gmail.com>
2018-03-13 17:25:13 -0400

    remove await parellel

--- Titanium.Web.Proxy/Extensions/FuncExtensions.cs
+++ Titanium.Web.Proxy/Extensions/FuncExtensions.cs
@@ -6,31 +6,6 @@ namespace Titanium.Web.Proxy.Extensions
 {
     internal static class FuncExtensions
     {
-        public static void InvokeParallel<T>(this AsyncEventHandler<T> callback, object sender, T args)
-        {
-            var invocationList = callback.GetInvocationList();
-            var handlerTasks = new Task[invocationList.Length];
-
-            for (int i = 0; i < invocationList.Length; i++)
-            {
-                handlerTasks[i] = ((AsyncEventHandler<T>)invocationList[i])(sender, args);
-            }
-
-            Task.WhenAll(handlerTasks).Wait();
-        }
-
-        public static async Task InvokeParallelAsync<T>(this AsyncEventHandler<T> callback, object sender, T args, Action<Exception> exceptionFunc)
-        {
-            var invocationList = callback.GetInvocationList();
-            var handlerTasks = new Task[invocationList.Length];
-
-            for (int i = 0; i < invocationList.Length; i++)
-            {
-                handlerTasks[i] = InternalInvokeAsync((AsyncEventHandler<T>)invocationList[i], sender, args, exceptionFunc);
-            }
-
-            await Task.WhenAll(handlerTasks);
-        }
 
         public static async Task InvokeAsync<T>(this AsyncEventHandler<T> callback, object sender, T args, Action<Exception> exceptionFunc)
         {
