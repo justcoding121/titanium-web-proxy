1b77bf221ae2927385c641b3f1a6b4ee3b4b28b0
Honfika <honfika@gmail.com>
2018-03-24 19:39:06 +0100

    decompression class caching

---
+++ Titanium.Web.Proxy/Compression/DefaultCompression.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Titanium.Web.Proxy.Compression
+{
+    /// <summary>
+    /// When no compression is specified just return the stream
+    /// </summary>
+    internal class DefaultCompression : ICompression
+    {
+        public async Task<byte[]> Compress(byte[] body)
+        {
+            return body;
+        }
+    }
+}
--- Titanium.Web.Proxy/Decompression/DecompressionFactory.cs
+++ Titanium.Web.Proxy/Decompression/DecompressionFactory.cs
@@ -1,22 +1,29 @@
-﻿namespace Titanium.Web.Proxy.Decompression
+﻿using System;
+
+namespace Titanium.Web.Proxy.Decompression
 {
     /// <summary>
     /// A factory to generate the de-compression methods based on the type of compression
     /// </summary>
     internal class DecompressionFactory
     {
-        public static DecompressionFactory Instance = new DecompressionFactory();
+        public static readonly DecompressionFactory Instance = new DecompressionFactory();
+
+        //cache
+        private static readonly Lazy<IDecompression> gzip = new Lazy<IDecompression>(() => new GZipDecompression());
+        private static readonly Lazy<IDecompression> deflate = new Lazy<IDecompression>(() => new DeflateDecompression());
+        private static readonly Lazy<IDecompression> def = new Lazy<IDecompression>(() => new DefaultDecompression());
 
         internal IDecompression Create(string type)
         {
             switch (type)
             {
                 case "gzip":
-                    return new GZipDecompression();
+                    return gzip.Value;
                 case "deflate":
-                    return new DeflateDecompression();
+                    return deflate.Value;
                 default:
-                    return new DefaultDecompression();
+                    return def.Value;
             }
         }
     }
