cfbafd374c0ba5fcf378b79321ec4a2eabed09b7
Honfika <honfika@gmail.com>
2018-03-25 09:17:09 +0200

    content length limit fix

--- Titanium.Web.Proxy/Compression/CompressionFactory.cs
+++ Titanium.Web.Proxy/Compression/CompressionFactory.cs
@@ -6,14 +6,11 @@ namespace Titanium.Web.Proxy.Compression
     /// <summary>
     ///  A factory to generate the compression methods based on the type of compression
     /// </summary>
-    internal class CompressionFactory
+    internal static class CompressionFactory
     {
-        public static readonly CompressionFactory Instance = new CompressionFactory();
-        
         //cache
         private static readonly Lazy<ICompression> gzip = new Lazy<ICompression>(() => new GZipCompression());
         private static readonly Lazy<ICompression> deflate = new Lazy<ICompression>(() => new DeflateCompression());
-        private static readonly Lazy<ICompression> def = new Lazy<ICompression>(() => new DefaultCompression());
 
         public static ICompression GetCompression(string type)
         {
@@ -24,7 +21,7 @@ namespace Titanium.Web.Proxy.Compression
                 case KnownHeaders.ContentEncodingDeflate:
                     return deflate.Value;
                 default:
-                    return def.Value;
+                    throw new Exception($"Unsupported compression mode: {type}");
             }
         }
     }
