db2b48a6e8fe683f30b7a0da2038e3305ea29d1f
Honfika <honfika@gmail.com>
2018-04-30 17:54:21 +0200

    simplify compressionfactory, usre stream reader interface when only the read methods are used, stream dispose in getbody

--- Titanium.Web.Proxy/Compression/DecompressionFactory.cs
+++ Titanium.Web.Proxy/Compression/DecompressionFactory.cs
@@ -1,26 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using Titanium.Web.Proxy.Http;
 
-namespace Titanium.Web.Proxy.Decompression
+namespace Titanium.Web.Proxy.Compression
 {
     /// <summary>
     ///     A factory to generate the de-compression methods based on the type of compression
     /// </summary>
     internal class DecompressionFactory
     {
-        //cache
-        private static readonly IDecompression gzip = new GZipDecompression();
-
-        private static readonly IDecompression deflate = new DeflateDecompression();
-
-        internal static IDecompression Create(string type)
+        internal static Stream Create(string type, Stream stream, bool leaveOpen = true)
         {
             switch (type)
             {
                 case KnownHeaders.ContentEncodingGzip:
-                    return gzip;
+                    return new GZipStream(stream, CompressionMode.Decompress, leaveOpen);
                 case KnownHeaders.ContentEncodingDeflate:
-                    return deflate;
+                    return new DeflateStream(stream, CompressionMode.Decompress, leaveOpen);
                 default:
                     throw new Exception($"Unsupported decompression mode: {type}");
             }
