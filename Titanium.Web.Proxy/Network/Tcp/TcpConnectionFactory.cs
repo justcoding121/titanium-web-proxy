f1655eafc82bf5db867631ac8038f2952c547c06
justcoding121 <jehonathant@gmail.com>
2018-05-10 10:11:46 -0400

    add comments

--- Titanium.Web.Proxy/Network/Tcp/TcpConnectionFactory.cs
+++ Titanium.Web.Proxy/Network/Tcp/TcpConnectionFactory.cs
@@ -342,6 +342,9 @@ namespace Titanium.Web.Proxy.Network.Tcp
         }
         /// <summary>
         ///     Check if a TcpClient is good to be used.
+        ///     This only checks if send is working so local socket is still connected.
+        ///     Receive can only be verified by doing a valid read from server without exceptions.
+        ///     So in our case we should retry with new connection from pool if first read after getting the connection fails.
         ///     https://msdn.microsoft.com/en-us/library/system.net.sockets.socket.connected(v=vs.110).aspx
         /// </summary>
         /// <param name="client"></param>
