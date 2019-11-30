4f43bd16c747de54f76c0651659b28002ddf5ffc
Honfika <honfika@gmail.com>
2019-11-30 21:39:35 +0100

    WebSocket IsFinal flad added

--- src/Titanium.Web.Proxy/WebSocketFrame.cs
+++ src/Titanium.Web.Proxy/WebSocketFrame.cs
@@ -5,6 +5,8 @@ namespace Titanium.Web.Proxy
 {
     public class WebSocketFrame
     {
+        public bool IsFinal { get; internal set; }
+        
         public WebsocketOpCode OpCode { get; internal set; }
 
         public ReadOnlyMemory<byte> Data { get; internal set; }
