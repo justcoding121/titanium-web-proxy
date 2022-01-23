542154bcf20940bbdb42e33c53bf471134be1377
honfika <honfika@gmail.com>
2022-01-23 23:44:55 +0100

    fix

--- src/Titanium.Web.Proxy/Certificates/Cache/DefaultCertificateDiskCache.cs
+++ src/Titanium.Web.Proxy/Certificates/Cache/DefaultCertificateDiskCache.cs
@@ -129,7 +129,7 @@ namespace Titanium.Web.Proxy.Network
                         assemblyLocation = Assembly.GetEntryAssembly().Location;
                     }
 
-                    // single-file app string.Empty location
+                    // single-file app returns string.Empty location
                     if (assemblyLocation == string.Empty)
                     {
                         assemblyLocation = AppContext.BaseDirectory;
