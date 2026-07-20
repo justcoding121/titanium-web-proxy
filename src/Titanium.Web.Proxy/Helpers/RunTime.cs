fb1977f2c422b6a0e98a5564973000641397456a
Jehonathan Thomas <jehonathant@gmail.com>
2026-07-20 14:23:10 -0600

    Upgrade to latest .net version

--- src/Titanium.Web.Proxy/Helpers/RunTime.cs
+++ src/Titanium.Web.Proxy/Helpers/RunTime.cs
@@ -13,7 +13,7 @@ public static class RunTime
 {
     private static readonly Lazy<bool> isRunningOnMono = new(() => Type.GetType("Mono.Runtime") != null);
 
-#if NET461
+#if NETFRAMEWORK
     /// <summary>
     ///     cache for Windows platform check
     /// </summary>
